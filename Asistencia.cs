7d542a7136539515c975205673512f5956e95979
BerlynSG <135674997+BerlynSG@users.noreply.github.com>
2026-07-14 16:58:08 -0400

    Crear la clase Asistencia
    Ejercicio 1: Verificar asistencia en lista
    Contexto:
    
    Tienes un arreglo con los nombres de los estudiantes inscritos. Un asistente desea saber si un estudiante llegó o no.
    
    Tarea:
    
    Dado un arreglo nombres[] y un nombreBuscado, usa búsqueda secuencial para determinar si está en la lista.

---
+++ Asistencia.cs
@@ -0,0 +1,20 @@
+class Asistencia
+{
+    public string[] nombres;
+
+    public Asistencia(string[] nombres)
+    {
+        this.nombres = nombres;
+    }
+    
+
+    public bool EstáPresente(string nombre)
+    {
+        for (int i = 0; i < nombres.Length; i++)
+        {
+            if (nombres[i] == nombre)
+                return true;
+        }
+        return false;
+    }
+}
